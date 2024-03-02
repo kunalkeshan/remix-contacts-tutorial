ee494dfdc522ed61cb773e97acc94bce687a7ac1
kunalkeshan <kunalkeshan12@gmail.com>
2024-03-02 20:13:52 +0530

    chore: added contacts flow by tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,55 +1,152 @@
 import {
-  Form,
-  Links,
-  LiveReload,
-  Meta,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Form,
+	Links,
+	LiveReload,
+	Meta,
+	Scripts,
+	Outlet,
+	ScrollRestoration,
+	NavLink,
+	useLoaderData,
+	useNavigation,
+	useSubmit,
+} from '@remix-run/react';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import appStylesHref from './app.css';
+import { useEffect } from 'react';
+
+import { getContacts, createEmptyContact } from './data';
+
+export const links: LinksFunction = () => [
+	{ rel: 'stylesheet', href: appStylesHref },
+];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const url = new URL(request.url);
+	const q = url.searchParams.get('q');
+	const contacts = await getContacts(q);
+	return json({ contacts, q });
+};
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+	const { contacts, q } = useLoaderData<typeof loader>();
+	const navigation = useNavigation();
+	const submit = useSubmit();
+	const searching =
+		navigation.location &&
+		new URLSearchParams(navigation.location.search).has('q');
 
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	useEffect(() => {
+		const searchField = document.getElementById('q');
+		if (searchField instanceof HTMLInputElement) {
+			searchField.value = q || '';
+		}
+	}, [q]);
+
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<div id='sidebar'>
+					<h1>Remix Contacts</h1>
+					<div>
+						<Form
+							id='search-form'
+							onChange={(event) => {
+								const isFirstSearch = q === null;
+								submit(event.currentTarget, {
+									replace: !isFirstSearch,
+								});
+							}}
+							role='search'
+						>
+							<input
+								id='q'
+								defaultValue={q || ''}
+								aria-label='Search contacts'
+								placeholder='Search'
+								type='search'
+								name='q'
+								className={searching ? 'loading' : ''}
+							/>
+							<div
+								id='search-spinner'
+								aria-hidden
+								hidden={!searching}
+							/>
+						</Form>
+						<Form method='post'>
+							<button type='submit'>New</button>
+						</Form>
+					</div>
+					<nav>
+						{contacts.length ? (
+							<ul>
+								{contacts.map((contact) => (
+									<li key={contact.id}>
+										<NavLink
+											to={`contacts/${contact.id}`}
+											className={({
+												isActive,
+												isPending,
+											}) =>
+												isActive
+													? 'active'
+													: isPending
+													? 'pending'
+													: ''
+											}
+										>
+											{contact.first || contact.last ? (
+												<>
+													{contact.first}{' '}
+													{contact.last}
+												</>
+											) : (
+												<i>No Name</i>
+											)}{' '}
+											{contact.favorite ? (
+												<span>★</span>
+											) : null}
+										</NavLink>
+									</li>
+								))}
+							</ul>
+						) : (
+							<p>
+								<i>No contacts</i>
+							</p>
+						)}
+					</nav>
+				</div>
+				<div
+					id='detail'
+					className={
+						navigation.state === 'loading' && !searching
+							? 'loading'
+							: ''
+					}
+				>
+					<Outlet />
+				</div>
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
+
+export const action = async () => {
+	const contact = await createEmptyContact();
+	return json({ contact });
+};
